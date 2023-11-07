f6c1327b4be9dda07ed79718742c0f14e4fc4831
abuyousuf75 <mdabuyousufsajib@gmail.com>
2023-11-08 00:52:24 +0600

    all jobs route added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express'); 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
  const app = express(); 
 const port = process.env.PORT || 5000;
- // middleware app.use(cors()); 
+ // middleware 
 app.use(express.json())
 app.use(cors()) 
 
@@ -27,22 +27,73 @@ async function run() {
     const webDevolopemtCollections = client.db('freeLand').collection('WebDevolopment');
     const graphicsDesignCollections = client.db('freeLand').collection('GraphicsDesign');
     const digitalMarketingCollections = client.db('freeLand').collection('DigitalMarketing');
+    const usersCollections = client.db('freeLand').collection('user');
+    const allBidJobsCollections =  client.db('freeLand').collection('allBid');
 
     app.get('/webDevolopment', async(req,res) =>{
         const cursor =  webDevolopemtCollections.find();
         const result = await cursor.toArray();
         res.send(result)
     })
+
+    // for webDevolopment specapics jobs
+    app.get('/webDevolopment/:id', async(req,res) =>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await webDevolopemtCollections.findOne(query);
+        res.send(result)
+        
+    })
+
     app.get('/digitalMarketing', async(req,res) =>{
         const cursor = digitalMarketingCollections.find();
         const result = await cursor.toArray();
         res.send(result)
     })
+
+// for DigitalMarketing specapics jobs
+app.get('/digitalMarketing/:id', async(req,res) =>{
+  const id = req.params.id;
+  const query = {_id : new ObjectId(id)};
+  const result = await digitalMarketingCollections.findOne(query);
+  res.send(result)
+  
+})
+
     app.get('/graphicsDesigner', async(req,res) =>{
         const cursor = graphicsDesignCollections.find();
         const result = await cursor.toArray();
         res.send(result)
     })
+// for graphicsDesigner specapics jobs
+app.get('/graphicsDesigner/:id', async(req,res) =>{
+  const id = req.params.id;
+  const query = {_id : new ObjectId(id)};
+  const result = await graphicsDesignCollections.findOne(query);
+  res.send(result)
+  
+})
+
+
+
+
+/// for all bid jobs
+
+app.post('/allBid', async(req,res) =>{
+  const bids = req.body;
+  console.log(bids)
+  const result = await allBidJobsCollections.insertOne(bids);
+  res.send(result)
+})
+
+
+    app.post('/user',async(req,res) =>{
+      const users = req.body;
+      console.log(users)
+      const result = await usersCollections.insertOne(users);
+      res.send(result)
+    })
+
 
 
 
